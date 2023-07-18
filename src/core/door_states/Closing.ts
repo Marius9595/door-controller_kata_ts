b76cd9365300eabedce625698e7699c2cefe0943
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2023-07-18 18:47:59 +0100

    🟢 door controller should change the closing to opening door when obstacle is detected

--- src/core/door_states/Closing.ts
+++ src/core/door_states/Closing.ts
@@ -1,12 +1,17 @@
 import { DoorState } from '../DoorState';
 import { DoorController } from '../DoorController';
 import { Closed } from './Closed';
+import { Opening } from "./Opening";
 
 export class Closing implements DoorState {
 	private paused = false;
 	constructor(private doorController: DoorController) {}
 	processEvents(events: string): string {
 		const doorIsFullyClosed = this.doorController.doorPosition == 0;
+		if (events[0] === 'O' && this.doorController.doorPosition > 0) {
+			this.doorController.changeState(new Opening(this.doorController));
+			return this.doorController.processEvents('.' + events.slice(1));
+		}
 		if (doorIsFullyClosed) {
 			this.doorController.changeState(new Closed(this.doorController));
 			return this.doorController.processEvents(events);
