b48b9f4f9d3e48b8da09065f05709bd3cf455d31
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2023-07-18 14:50:58 +0100

    :recycle: Renamed variable

--- src/core/door_states/Opening.ts
+++ src/core/door_states/Opening.ts
@@ -3,16 +3,16 @@ import { DoorController } from '../DoorController';
 import { Opened } from './Opened';
 
 export class Opening implements DoorState {
-	private position = 0;
+	private doorPosition = 0;
 	constructor(private doorController: DoorController) {}
 	processEvents(events: string): string {
-		const doorIsFullyOpen = this.position == 5;
+		const doorIsFullyOpen = this.doorPosition == 5;
 		if (doorIsFullyOpen) {
 			this.doorController.changeState(new Opened(this.doorController));
 			return this.doorController.processEvents(events);
 		}
-		this.position++;
-		const processedEvent = this.position.toString();
+		this.doorPosition++;
+		const processedEvent = this.doorPosition.toString();
 		const lastEventToProcess = events.length === 1;
 		if (lastEventToProcess) {
 			return processedEvent;
