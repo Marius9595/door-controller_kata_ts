4ee1fe81cedf35a3531799905d813f778b5cb81c
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2023-07-18 14:49:06 +0100

    🟢 door controller should start close the door when this is opened and button is pressed

--- src/core/door_states/Opened.ts
+++ src/core/door_states/Opened.ts
@@ -4,6 +4,12 @@ import { DoorController } from '../DoorController';
 export class Opened implements DoorState {
 	constructor(private doorController: DoorController) {}
 	processEvents(events: string): string {
-		return '5'.repeat(events.length);
+		if (events[0] === 'P') {
+			return '4';
+		}
+		if (events.length === 1) {
+			return '5';
+		}
+		return '5' + this.doorController.processEvents(events.slice(1));
 	}
 }
