8f55510330ea0f7cf28be5b81b52024ec9048c83
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2023-07-18 14:36:29 +0100

    🟢 door controller should open the door completely when this was closed and button was pressed

--- src/core/door_states/Opening.ts
+++ src/core/door_states/Opening.ts
@@ -2,8 +2,16 @@ import { DoorState } from '../DoorState';
 import { DoorController } from '../DoorController';
 
 export class Opening implements DoorState {
+	private position = 0;
 	constructor(private doorController: DoorController) {}
 	processEvents(events: string): string {
-		return '1';
+		this.position++;
+		const processedEvent = this.position.toString();
+		const lastEventToProcess = events.length === 1;
+		if (lastEventToProcess) {
+			return processedEvent;
+		}
+		const restEventsToProcess = events.slice(1);
+		return processedEvent + this.processEvents(restEventsToProcess);
 	}
 }
