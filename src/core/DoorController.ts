d25de321311b54f97c2fa8d1ace29f5b6063b711
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2023-07-18 14:11:51 +0100

    🟢 door controller should start open the door when this is closed and button is pressed

--- src/core/DoorController.ts
+++ src/core/DoorController.ts
@@ -1,8 +1,12 @@
 export class DoorController {
 	processEvents(events: string) {
-		if (events.length === 1) {
-			return '0';
+		let processedEvent = '0';
+		if (events[0] == 'P') {
+			processedEvent = '1';
 		}
-		return '0' + this.processEvents(events.slice(1));
+		if (events.length === 1) {
+			return processedEvent;
+		}
+		return processedEvent + this.processEvents(events.slice(1));
 	}
 }
